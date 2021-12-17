c39aaf794541a11a5884b01464fe5ec668bf5eae
unknown <neetamourya24@gmail.com>
2021-12-17 22:41:03 +0530

    [Neeta mourya] welcome message in master branch

---
+++ README.md
@@ -0,0 +1,1 @@
+"# DeckOfCardsDay18" 
---
+++ src/main/java/com/bridgelabz/datastructure/deckofcards/DeckOFCards.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructure.deckofcards;
+
+public class DeckOFCards {
+	public static void main(String[] args) {
+		System.out.println("Welcome to deck of cards");
+	}
+
+}
